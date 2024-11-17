ecc0c2a00badf1c65a2dac3a198c69bf8f4870f3
Alakinnn <s3978452@rmit.edu.vn>
2024-11-17 23:33:54 +0700

    add currency model

---
+++ CurrencyConverterApp/models/Currency.swift
@@ -0,0 +1,24 @@
+//
+//  Currency.swift
+//  CurrencyConverterApp
+//
+//  Created by Duong Tran Minh Hoang on 17/11/2024.
+//
+
+import Foundation
+
+enum Currency: String, CaseIterable, Codable {
+  case usd = "USD"
+  case eur = "EUR"
+  case gdp = "GDP"
+  case jpy = "JPY"
+  
+  var symbol: String {
+    switch self {
+    case .usd: return "$"
+    case .eur: return "€"
+    case .gdp: return "£"
+    case .jpy: return "¥"
+    }
+  }
+}
