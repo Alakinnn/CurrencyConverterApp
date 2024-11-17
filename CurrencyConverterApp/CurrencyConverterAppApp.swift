0aafeee70161436a3af79ca7df538a10869aae23
Alakinnn <s3978452@rmit.edu.vn>
2024-11-17 23:01:59 +0700

    Initial Commit

---
+++ CurrencyConverterApp/CurrencyConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterAppApp.swift
+//  CurrencyConverterApp
+//
+//  Created by Duong Tran Minh Hoang on 17/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
