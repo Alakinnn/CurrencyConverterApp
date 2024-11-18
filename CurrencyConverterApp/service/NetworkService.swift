e6cbdd3ddd8d848d187fa122f87a0c011e4fbf96
Alakinnn <s3978452@rmit.edu.vn>
2024-11-18 12:26:49 +0700

    update error handling enum

--- CurrencyConverterApp/service/NetworkService.swift
+++ CurrencyConverterApp/service/NetworkService.swift
@@ -9,20 +9,26 @@ import Foundation
 
 actor NetworkService {
   enum NetworkError: LocalizedError {
-      case invalidURL
-      case invalidResponse
-      case apiError(String)
-      
-      var errorDescription: String? {
-          switch self {
-          case .invalidURL:
-              return "Invalid URL"
-          case .invalidResponse:
-              return "Invalid response from server"
-          case .apiError(let message):
-              return message
-          }
+    case invalidURL
+    case invalidResponse
+    case apiError(String)
+    
+    case unknownCurrencyCode
+    case conversionError(String)
+    
+    var errorDescription: String? {
+      switch self {
+      case .invalidURL:
+        return "Invalid URL"
+      case .invalidResponse:
+        return "Invalid response from server"
+      case .apiError(let message):
+        return message
+      case .unknownCurrencyCode:
+        return "Unknown currency code"
+      case .conversionError(let message):
+        return message
       }
+    }
   }
-  
 }
