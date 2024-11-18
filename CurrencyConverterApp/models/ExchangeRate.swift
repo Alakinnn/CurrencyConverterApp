af6b13f262d4fefc574902a67d0595c015fbcd44
Alakinnn <s3978452@rmit.edu.vn>
2024-11-18 12:24:44 +0700

    add enum class for JSON de/en-code

--- CurrencyConverterApp/models/ExchangeRate.swift
+++ CurrencyConverterApp/models/ExchangeRate.swift
@@ -21,4 +21,20 @@ struct ExchangeRate: Codable {
   let timeLastUpdateUtc: String?
   let timeNextUpdateUnix: TimeInterval?
   let timeNextUpdateUtc: String?
+  
+//  JSON keys to ExchangeRate class response
+  enum CodingKeys: String, CodingKey {
+    case result
+    case conversionRate = "conversion_rate"
+    case errorType = "error-type"
+    case baseCode = "base_code"
+    case targetCode = "target_code"
+    case conversionResult = "conversion_result"
+    case documentation
+    case termsOfUse = "terms_of_use"
+    case timeLastUpdateUnix = "time_last_update_unix"
+    case timeLastUpdateUtc = "time_last_update_utc"
+    case timeNextUpdateUnix = "time_next_update_unix"
+    case timeNextUpdateUtc = "time_next_update_utc"
+  }
 }
