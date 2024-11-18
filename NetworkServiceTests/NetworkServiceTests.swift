8f830ee4f8d1c26fa219f070347bba2cbe553576
Alakinnn <s3978452@rmit.edu.vn>
2024-11-18 13:44:24 +0700

    create test class for network service

--- NetworkServiceTests/NetworkServiceTests.swift
+++ NetworkServiceTests/NetworkServiceTests.swift
@@ -6,100 +6,88 @@
 //
 
 import XCTest
-@testable import CurrencyConverterApp // Make sure this matches your app name
+@testable import CurrencyConverterApp
 
-// First create the MockURLSession
-class MockURLSession: URLSession {
-    var mockData: Data?
-    var mockResponse: URLResponse?
-    var mockError: Error?
+// This file is AI-Generated
+class MockURLProtocol: URLProtocol {
+    static var mockData: Data?
+    static var mockResponse: URLResponse?
+    static var mockError: Error?
     
-    override func data(
-        from url: URL,
-        delegate: URLSessionTaskDelegate? = nil
-    ) async throws -> (Data, URLResponse) {
-        if let mockError = mockError {
-            throw mockError
-        }
-        return (
-            mockData ?? Data(),
-            mockResponse ?? URLResponse()
-        )
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
     }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        if let error = MockURLProtocol.mockError {
+            client?.urlProtocol(self, didFailWithError: error)
+            return
+        }
+        
+        if let response = MockURLProtocol.mockResponse {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+        
+        if let data = MockURLProtocol.mockData {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        
+        client?.urlProtocolDidFinishLoading(self)
+    }
+    
+    override func stopLoading() {}
 }
 
-// Then your test class
 final class NetworkServiceTests: XCTestCase {
-    private var sut: NetworkService!
-    private var mockURLSession: MockURLSession!
+  private var sut: NetworkService!
+     private var session: URLSession!
+     
+     override func setUp() {
+         super.setUp()
+         let configuration = URLSessionConfiguration.ephemeral
+         configuration.protocolClasses = [MockURLProtocol.self]
+         session = URLSession(configuration: configuration)
+         
+         sut = NetworkService(session: session)
+     }
+     
+     override func tearDown() {
+         sut = nil
+         session = nil
+         MockURLProtocol.mockData = nil
+         MockURLProtocol.mockResponse = nil
+         MockURLProtocol.mockError = nil
+         super.tearDown()
+     }
     
-    override func setUp() {
-        super.setUp()
-        mockURLSession = MockURLSession()
-        sut = NetworkService(session: mockURLSession)
-    }
-    
-    override func tearDown() {
-        sut = nil
-        mockURLSession = nil
-        super.tearDown()
-    }
-    
-    func testFetchExchangeRate_Success() async throws {
-        // Given
-        let expectedRate = 0.8412
-        let mockResponse = """
-        {
-            "result": "success",
-            "documentation": "https://www.exchangerate-api.com/docs",
-            "terms_of_use": "https://www.exchangerate-api.com/terms",
-            "time_last_update_unix": 1585267200,
-            "time_last_update_utc": "Fri, 27 Mar 2020 00:00:00 +0000",
-            "time_next_update_unix": 1585270800,
-            "time_next_update_utc": "Sat, 28 Mar 2020 01:00:00 +0000",
-            "base_code": "EUR",
-            "target_code": "GBP",
-            "conversion_rate": \(expectedRate),
-            "conversion_result": 5.8884
-        }
-        """
-        mockURLSession.mockData = mockResponse.data(using: .utf8)
-        mockURLSession.mockResponse = HTTPURLResponse(
-            url: URL(string: "https://test.com")!,
-            statusCode: 200,
-            httpVersion: nil,
-            headerFields: nil
-        )
-        
-        // When
-        let rate = try await sut.fetchExchangeRate(from: .eur, to: .gbp)
-        
-        // Then
-        XCTAssertEqual(rate, expectedRate)
-    }
-    
-    func testFetchExchangeRate_UnknownCode() async throws {
-        // Given
-        let mockResponse = """
-        {
-            "result": "error",
-            "error-type": "unknown-code"
-        }
-        """
-        mockURLSession.mockData = mockResponse.data(using: .utf8)
-        mockURLSession.mockResponse = HTTPURLResponse(
-            url: URL(string: "https://test.com")!,
-            statusCode: 200,
-            httpVersion: nil,
-            headerFields: nil
-        )
-        
-        // When/Then
-        do {
-          _ = try await sut.fetchExchangeRate(from: .eur, to: .gbp)
-            XCTFail("Expected error wasn't thrown")
-        } catch let error as NetworkService.NetworkError {
-            XCTAssertEqual(error, .unknownCurrencyCode)
-        }
-    }
-}
+  func testFetchExchangeRate_UnknownCode() async throws {
+          // Given
+          let mockResponse = """
+          {
+              "result": "error",
+              "error-type": "unknown-code"
+          }
+          """
+          
+          // Use MockURLProtocol instead of MockURLSession
+          MockURLProtocol.mockData = mockResponse.data(using: .utf8)
+          MockURLProtocol.mockResponse = HTTPURLResponse(
+              url: URL(string: "https://test.com")!,
+              statusCode: 200,
+              httpVersion: nil,
+              headerFields: nil
+          )
+          
+          // When/Then
+          do {
+              _ = try await sut.fetchExchangeRate(from: .eur, to: .gbp)
+              XCTFail("Expected error wasn't thrown")
+          } catch let error as NetworkService.NetworkError {
+              XCTAssertEqual(error, .unknownCurrencyCode)
+          }
+      }
+  }
