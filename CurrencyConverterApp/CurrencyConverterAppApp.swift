8ebc192e615778d99066045725beddcb64a16821
Alakinnn <s3978452@rmit.edu.vn>
2024-11-18 17:12:40 +0700

    add memory-state object

--- CurrencyConverterApp/CurrencyConverterAppApp.swift
+++ CurrencyConverterApp/CurrencyConverterAppApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct CurrencyConverterAppApp: App {
+  @State private var appState = AppState.shared
     var body: some Scene {
         WindowGroup {
             ContentView()
+            .environment(appState)
         }
     }
 }
